ad8ace457ce5f0ba58f9ea746156b07a043b489e
Di-key <vfantomkhayv@mail.ru>
2024-01-28 10:33:12 +0300

    commit

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Ocean1 {
+public class  Ocean1 {
 
-    @Autowired
-    private Island2 island;
+    private final Island2 island;
+
+    public Ocean1(Island2 island) {
+        this.island = island;
+    }
 
     @Override
     public String toString() {
